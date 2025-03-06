0a2ed8363501cd1ae2bc0861b8e7350cffea85f6
Tochukwu Charles Ikechukwu <ikechukwutochukwu95@gmail.com>
2025-03-06 12:30:08 +0100

    GETTING THE AVERAGE OF COST OF IEMS

--- week3Practice.js
+++ week3Practice.js
@@ -33,4 +33,43 @@ console.log(MyBestQuote.toLocaleUpperCase());
 console.log(MyBestQuote.toLocaleLowerCase());
 
 
+// ASSIGNMENT 3: Take a word and rearrange it in reverse order, then log the reversed word.
+
+
+// Creating a string binding
+let Word = "Personality"
+
+//reversing the string and logging it out
+let reverseWord = Word.split("").reverse().join("");
+
+console.log(reverseWord);
+
+
+// ASSIGNMENT 4.Store the price of 3 items in 3 different variables.Calculate the total and display it with a message.
+
+// Creating a binding for food items and getting their combined total price
+var ricePrice = 4000
+var tomatoesPrice = 300
+var beansPrice = 3500
+var totalPrice = ricePrice + tomatoesPrice + beansPrice
+
+var expenseMessage = `Your total purchase cost #${totalPrice}, thanks for the patronage`
+
+console.log(expenseMessage);
+
+
+
+//Assignment 5: Find the average of 5 test scores stored in variables. Display the average with a message.
+
+//creating the variables
+var mathScore = 80;
+var EnglishScore = 60;
+var GovernmentScore = 85;
+var physicsScore = 75;
+var commerceScore = 89;
+
+var averageScore = mathScore + EnglishScore + GovernmentScore + physicsScore + commerceScore / 2;
+var averageScoreMessage = `Your average score for the 5 tests is ${averageScore}, keep pushing yourself!`
+console.log(averageScoreMessage);
+
 
