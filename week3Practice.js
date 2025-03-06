cc61233ce88f584dec81cdff3b02a13eb1fdbf49
Tochukwu Charles Ikechukwu <ikechukwutochukwu95@gmail.com>
2025-03-06 12:28:04 +0100

    reversing a string

--- week3Practice.js
+++ week3Practice.js
@@ -1,5 +1,4 @@
-// Assignment 1
-// variables to store my name, age, and nationality. Then combining them into a sentence that introduces me.
+// Assignment 1 Create variables to store your name, age, and nationality. Then combine them into a sentence that introduces you.
 
 //creating a binding for my name
 const myName = "Tochukwu"
@@ -17,3 +16,21 @@ let myIntroduction = `Hello my name is ${myName}, I am from ${myNationality} and
 
 //Displaying the myIntroduction variable as a sentence. 
 console.log(myIntroduction)
+
+
+// ASSIGNMENT 2:  Make a variable with your favorite quote or lyric.Change the entire text to uppercase and lowercase in separate steps.
+
+
+// creating a binding for my favourite quote
+var MyBestQuote = "To see the hand of God in all situations"
+
+
+//logging out the 'MyBestQuote' string content as upper case
+console.log(MyBestQuote.toLocaleUpperCase());
+
+
+//logging out the 'MyBestQuote' string content as lower case.
+console.log(MyBestQuote.toLocaleLowerCase());
+
+
+
