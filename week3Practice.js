f5404a0c5852ab13d7edd59abdbbe0b8ecab23cf
Tochukwu Charles Ikechukwu <ikechukwutochukwu95@gmail.com>
2025-03-06 10:04:11 +0100

    variables to store my name, age, and nationality. Then combining them into a sentence that introduces me.

---
+++ week3Practice.js
@@ -0,0 +1,19 @@
+// Assignment 1
+// variables to store my name, age, and nationality. Then combining them into a sentence that introduces me.
+
+//creating a binding for my name
+const myName = "Tochukwu"
+
+//creating a binding for my myAge
+var myAge = 30
+
+//creating a binding for my country
+var myNationality = "Nigeria"
+
+
+//creating a binding for an introductory sence aboout me
+let myIntroduction = `Hello my name is ${myName}, I am from ${myNationality} and I am ${myAge} years old, thanks`
+
+
+//Displaying the myIntroduction variable as a sentence. 
+console.log(myIntroduction)
