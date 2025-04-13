c0298c0232fed3a686104ec971f74a95d860f126
Tochukwu Charles Ikechukwu <ikechukwutochukwu95@gmail.com>
2025-04-13 09:03:49 +0200

    initial commit

---
+++ week6/index.js
@@ -0,0 +1,9 @@
+//this code calculates the answer of 2 to the power of
+counter = 0;
+beginner = 2;
+while (counter <= 10) {
+    beginner = beginner * 2
+    counter++;
+}
+
+console.log(`the answer to 2 raised to the power of ${counter} is ${beginner}`)
